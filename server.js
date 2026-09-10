bfe626d21d32a2679e9e3e36870027d50ee922da
Uvissh <vishalbhateria@gmail.com>
2026-09-11 00:20:09 +0530

    commit in cors

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "https://eventbookingfrontend1.onrender.com",
-    credentials:true
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
 app.use(userRouter);
 app.use(seatrouter);
